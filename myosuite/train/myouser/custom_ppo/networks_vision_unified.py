0f47975482a624a591edb4cca57a68c78c9cf348
Ankit Bhattarai <ankitbhattarai.ab@protonmail.com>
2025-07-11 20:02:29 +0100

    add cheat feature to vision encoder

--- myosuite/train/myouser/custom_ppo/networks_vision_unified.py
+++ myosuite/train/myouser/custom_ppo/networks_vision_unified.py
@@ -117,8 +117,7 @@ class PPONetworksUnifiedVision(nnx.Module):
         only_vision_aux_feature: bool = False,
     ):
         if self.has_vision:
-            vision_obs = {k: v for k, v in obs.items() if k.startswith("pixels/")}
-            vision_feature = self.vision_encoder(vision_obs)
+            vision_feature = self.vision_encoder(obs)
             if only_vision_aux_feature:
                 return {"vision_aux_vector": self.vision_aux_output(vision_feature)}
             gradient_stopped_vision_feature = jax.lax.stop_gradient(vision_feature)
@@ -297,41 +296,63 @@ class VisionEncoder(nnx.Module):
     def __init__(
         self,
         rngs: nnx.Rngs,
-        vision_out_size: int = 7200,
+        c_hid: int = 32,
+        vision_out_size: int = 14400,
+        mlp_between_size: int = 256,
         mlp_out_size: int = 4,
-        mlp_hidden_layers: Optional[Sequence[int]] = [256],
+        mlp_hidden_layers: Optional[Sequence[int]] = [256, 128, 64, 32],
+        cheat_vision_aux_output: bool = False,
         use_bias: bool = True,
         activation: Callable = nnx.leaky_relu,
     ):
+        if cheat_vision_aux_output:
+            self.cheat_vision_aux_output = True
+            return
+        else:
+            self.cheat_vision_aux_output = False
         self.conv1 = nnx.Conv(
-            1, 8, kernel_size=(3, 3), strides=(2, 2), padding=(1, 1), rngs=rngs
+            1, c_hid, kernel_size=(3, 3), strides=2, rngs=rngs, use_bias=use_bias
         )
         self.conv2 = nnx.Conv(
-            8, 16, kernel_size=(3, 3), strides=(2, 2), padding=(1, 1), rngs=rngs
+            c_hid, c_hid, kernel_size=(3, 3), rngs=rngs, use_bias=use_bias
         )
         self.conv3 = nnx.Conv(
-            16, 32, kernel_size=(3, 3), strides=(2, 2), padding=(1, 1), rngs=rngs
+            c_hid, 2 * c_hid, kernel_size=(3, 3), strides=2, rngs=rngs, use_bias=use_bias
         )
-        self.mlp = MLP(
-            vision_out_size,
-            mlp_out_size,
-            rngs=rngs,
-            hidden_layers=mlp_hidden_layers,
-            use_bias=use_bias,
-            activation=activation,
+        self.conv4 = nnx.Conv(
+            2 * c_hid, 2 * c_hid, kernel_size=(3, 3), rngs=rngs, use_bias=use_bias
         )
+        self.conv5 = nnx.Conv(
+            2 * c_hid, 2 * c_hid, kernel_size=(3, 3), strides=2, rngs=rngs, use_bias=use_bias
+        )
+        self.linear1 = nnx.Linear(vision_out_size, mlp_between_size, rngs=rngs)
+        self.linear2 = nnx.Linear(mlp_between_size, mlp_out_size, rngs=rngs)
+        # self.mlp = MLP(
+        #     vision_out_size,
+        #     mlp_out_size,
+        #     rngs=rngs,
+        #     hidden_layers=mlp_hidden_layers,
+        #     use_bias=use_bias,
+        #     activation=activation,
+        # )
         self.activation = activation
 
-    def __call__(self, x: jnp.ndarray):
+    def __call__(self, x: dict):
+        if self.cheat_vision_aux_output:
+            return x['vision_aux_targets']
         vision_keys = [k for k in x.keys() if k.startswith("pixels/")]
         assert len(vision_keys) == 1, "Only one vision key is supported"
         x = x[vision_keys[0]]
         x = self.activation(self.conv1(x))
         x = self.activation(self.conv2(x))
         x = self.activation(self.conv3(x))
+        x = self.activation(self.conv4(x))
+        x = self.activation(self.conv5(x))
         spatial_dims = x.ndim - 3
         x = x.reshape(*x.shape[:spatial_dims], -1)
-        x = self.mlp(x)
+        x = self.activation(self.linear1(x))
+        x = self.linear2(x)
+        # x = self.mlp(x)
         return x
 
 
@@ -371,6 +392,7 @@ class NetworkWithVision(PPONetworksUnifiedVision):
         encoder_out_size: int,
         preprocess_observations_fn: Callable,
         rngs: nnx.Rngs,
+        cheat_vision_aux_output: bool = False,
     ):
         states_combiner = StatesCombinerPredictStateVariables(
             preprocess_observations_fn
@@ -389,7 +411,7 @@ class NetworkWithVision(PPONetworksUnifiedVision):
             event_size=action_size
         )
         proprioception_obs_key = "proprioception"
-        vision_encoder = VisionEncoder(rngs=rngs)
+        vision_encoder = VisionEncoder(rngs=rngs, cheat_vision_aux_output=cheat_vision_aux_output)
         vision_aux_output = VisionAuxOutputIdentity(rngs=rngs)
         states_combiner = StatesCombinerPredictStateVariables(
             preprocess_observations_fn
@@ -410,6 +432,7 @@ def make_ppo_networks_with_vision(
     action_size: int,
     encoder_out_size: int,
     preprocess_observations_fn: Callable,
+    cheat_vision_aux_output: bool = False,
 ):
     model = nnx.bridge.to_linen(
         NetworkWithVision,
@@ -417,5 +440,6 @@ def make_ppo_networks_with_vision(
         action_size=action_size,
         encoder_out_size=encoder_out_size,
         preprocess_observations_fn=preprocess_observations_fn,
+        cheat_vision_aux_output=cheat_vision_aux_output,
     )
     return model
