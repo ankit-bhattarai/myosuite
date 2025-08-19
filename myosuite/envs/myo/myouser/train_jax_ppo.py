00780a88598599e2dc0b3b481c2d32cc9bfa532f
HannahSelder <hannah.selder@web.de>
2025-08-19 17:00:36 +0200

    remove wandb entity

--- myosuite/envs/myo/myouser/train_jax_ppo.py
+++ myosuite/envs/myo/myouser/train_jax_ppo.py
@@ -452,7 +452,6 @@ def main(argv):
   print(f"Environment Config:\n{env_cfg}")
   print(f"PPO Training Parameters:\n{ppo_params}")
 
-
   # Generate unique experiment name
   now = datetime.now()
   timestamp = now.strftime("%Y%m%d-%H%M%S")
@@ -468,7 +467,7 @@ def main(argv):
 
   # Initialize Weights & Biases if required
   if _USE_WANDB.value and not _PLAY_ONLY.value:
-    wandb.init(project="mjxrl", entity="bayreuth", name=exp_name)
+    wandb.init(project="mjxrl", name=exp_name)
     wandb.config.update(env_cfg.to_dict())
     wandb.config.update({"env_name": _ENV_NAME.value})
 
