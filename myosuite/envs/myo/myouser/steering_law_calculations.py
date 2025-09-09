68c8ab0f30ebfb544bd13ade61bc88d5c83265fa
HannahSelder <hannah.selder@web.de>
2025-09-09 14:27:14 +0200

    Add else case for MT means_

--- myosuite/envs/myo/myouser/steering_law_calculations.py
+++ myosuite/envs/myo/myouser/steering_law_calculations.py
@@ -56,7 +56,7 @@ def preprocess_steering_law_rollouts(movement_times, rollout_states, task, avera
         sl_data = {"ID": IDs, "MT_ref": movement_times,
                     "D": Ds, "W": Ws, "X": Xs, "Y": Ys}
     elif task in ('varying_width',):
-        IDs = np.array([np.abs(r.info["tunnel_extras"]["ID"]) for r in rollout_states])
+        IDs = np.array([np.abs(r.info["tunnel_extras"]["ID"]) for r in rollout_states]).reshape(-1, 1)
         Xs = np.array([r.info["tunnel_nodes"][:,0] for r in rollout_states])
         Ys = np.array([r.info["tunnel_nodes"][:,1] for r in rollout_states])
         sl_data = {"ID": IDs, "MT_ref": movement_times, "X": Xs, "Y": Ys}
@@ -79,7 +79,7 @@ def average_movement_times_per_path(sl_data, outlier_std=None, outlier_proportio
     results = {k: [] for k in sl_data.keys()}
     results["ID_means"] = []
     results["MT_ref_means"] = []
-    
+
     for _, idxs in groups.items():
         idx = idxs[0]
         for k in sl_data.keys():
@@ -87,23 +87,30 @@ def average_movement_times_per_path(sl_data, outlier_std=None, outlier_proportio
                 results[k].append(np.array(sl_data[k][idx]))
         
         mt_vals = np.array(sl_data["MT_ref"])[idxs]
-        if outlier_std is not None:
-            mt_vals_wo_outliers = mt_vals[abs(scipy.stats.zscore(mt_vals)) <= outlier_std]
+        if mt_vals.size > 2:
+            if outlier_std is not None:
+                mt_vals_wo_outliers = mt_vals[abs(scipy.stats.zscore(mt_vals)) <= outlier_std]
+            else:
+                mt_vals_wo_outliers = mt_vals.copy()
+            if outlier_proportiontocut is not None:
+                mt_vals_wo_outliers = scipy.stats.trimboth(mt_vals_wo_outliers, proportiontocut=outlier_proportiontocut)
         else:
-            mt_vals_wo_outliers = mt_vals.copy()
-        if outlier_proportiontocut is not None:
-            mt_vals_wo_outliers = scipy.stats.trimboth(mt_vals_wo_outliers, proportiontocut=outlier_proportiontocut)
+            mt_vals_wo_outliers = mt_vals
+
         mt_mean = np.mean(mt_vals_wo_outliers)
         results["MT_ref_means"].append(mt_mean)
         results["MT_ref"].append(np.array(sl_data["MT_ref"])[idxs])
 
         id_vals = sl_data["ID"][idxs]
-        if outlier_std is not None:
-            id_vals_wo_outliers = id_vals[abs(scipy.stats.zscore(mt_vals)) <= outlier_std]
+        if id_vals.size > 2:
+            if outlier_std is not None:
+                id_vals_wo_outliers = id_vals[abs(scipy.stats.zscore(mt_vals)) <= outlier_std]
+            else:
+                id_vals_wo_outliers = id_vals.copy()
+            if outlier_proportiontocut is not None:
+                id_vals_wo_outliers = scipy.stats.trimboth(id_vals_wo_outliers, proportiontocut=outlier_proportiontocut)  #TODO: fix (only works if all entries of id_vals_wo_outliers are the same)!
         else:
-            id_vals_wo_outliers = id_vals.copy()
-        if outlier_proportiontocut is not None:
-            id_vals_wo_outliers = scipy.stats.trimboth(id_vals_wo_outliers, proportiontocut=outlier_proportiontocut)  #TODO: fix (only works if all entries of id_vals_wo_outliers are the same)!
+            id_vals_wo_outliers = id_vals
         id_mean = np.mean(id_vals_wo_outliers)
         results["ID_means"].append(id_mean)
         results["ID"].append(sl_data["ID"][idxs])
@@ -354,7 +361,7 @@ def calculate_original_steering_law(sl_data, average_r2=True):
     if len(IDs) == 0 or len(MTs) == 0:
         return np.nan, np.nan, np.nan, np.nan
     
-    a, b, r2, y_pred = fit_model(IDs, MTs)
+    a, b, r2, y_pred = fit_model(np.array(IDs).reshape(-1,1), np.array(MTs))
 
     print(f"R^2: {r2}, a,b: {a},{b}")
 
